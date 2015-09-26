90ec26a275e607b45d27f6320e1aaa369c4d114d
lepetitrobot <lepetit_robot@yahoo.co.uk>
2015-09-26 12:25:48 +0100

    changed argument to being x for 2nd function

--- cachematrix.R
+++ cachematrix.R
@@ -21,8 +21,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## if the inverse of the matrix x has not already been cached then cacheinverse will calculate and print it out
+##x is the output of makeCacheMatrix
 
-cacheinverse <- function(makeCacheMatrix(x), ...) {
+cacheinverse <- function(x, ...) {
   m <- x$getinverse()
   if(!is.null(m)) {
     message("getting cached data")
